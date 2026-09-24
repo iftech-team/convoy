bb636faa50fd7cab8bad54881aeeffc2ee4df006
ifteakhor <saidkhojaiftikhor2002@gmail.com>
2026-09-24 11:07:39 +0500

    chore: version 0.6.0

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -40,8 +40,8 @@ cat > "$app/Contents/Info.plist" <<'PLIST'
 <key>CFBundleDisplayName</key><string>Convoy</string>
 <key>CFBundleIconFile</key><string>AppIcon</string>
 <key>CFBundlePackageType</key><string>APPL</string>
-<key>CFBundleShortVersionString</key><string>0.5.9</string>
-<key>CFBundleVersion</key><string>14</string>
+<key>CFBundleShortVersionString</key><string>0.6.0</string>
+<key>CFBundleVersion</key><string>15</string>
 <key>LSMinimumSystemVersion</key><string>14.0</string>
 <key>NSHighResolutionCapable</key><true/>
 </dict></plist>
